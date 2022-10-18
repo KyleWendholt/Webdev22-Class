8303b7211ecac6036029c1a400f692e546d593f3
KyleWendholt <101617205+KyleWendholt@users.noreply.github.com>
2022-10-18 18:10:15 -0400

    Work done to change pages and add traversal

---
+++ client/src/components/LoginBadge.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="buttons" v-if="session.user == null">
+    <a class="button is-light" @click="login("Kyle", 'Wendholt')">
+    Log in
+  </a>
+  </div>
+  <div v-else>
+    Welcome{{}}
+  </div>
+</template>
+
+<script setup lang="ts">
+  import session,{ login } from '../stores/session';
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/views/StoreView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1 class="Title">Store</h1>
+    <p>Store View</p>
+  </div>
+
+</template>
+<style scoped></style>
