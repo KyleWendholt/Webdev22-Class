f6e1331ec03617d8f6873cf3484d425fcc01dbc1
KyleWendholt <101617205+KyleWendholt@users.noreply.github.com>
2022-10-31 15:16:05 -0400

    starting server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 
+const productsController = require('./controllers/products');
+
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Happy Sweet New Year');
-})
+    res.status(200).send('Hello World!')
+}).use('api/v1/prodcuts', productsController)
 
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
