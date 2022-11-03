5f0573dfc65d85be060fdfa0600e97be90b4dea2
KyleWendholt <101617205+KyleWendholt@users.noreply.github.com>
2022-11-03 15:14:00 -0400

    class work from 11/3

--- client/src/components/LoginBadge.vue
+++ client/src/components/LoginBadge.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="buttons" v-if="session.user == null">
-    <a class="button is-light" @click="login("Kyle", 'Wendholt')">
+    <a class="button is-light" @click="login('Kyle', 'Wendholt')">
     Log in
   </a>
   </div>
--- server/index.js
+++ server/index.js
@@ -1,15 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const productsController = require('./controllers/products');
+const productsController = require("./controllers/products");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World!')
-}).use('api/v1/prodcuts', productsController)
+app.use((req, res, next) => {
+  res.setHeader("SUNY", "my suny");
+  next();
+});
+app.use("/", express.static("./client/dist"));
+
+app
+  .get("/", (req, res) => {
+    res.status(200).send("Hello World!");
+  })
+  .get("/error", (req, res) => {
+    throw new Error("Error!");
+  })
+  .use("api/v1/prodcuts", productsController);
+
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root: "./client/dist" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.httpCode ?? 500).send({
+    message: err.msg ?? "Something broke!",
+    status: err.httpCode ?? 500,
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
