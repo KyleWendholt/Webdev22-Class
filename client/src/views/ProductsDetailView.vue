360c959bc70e0524cc4bb3a0d81b662016ae0d9b
KyleWendholt <101617205+KyleWendholt@users.noreply.github.com>
2022-11-07 14:50:51 -0500

    updated to current products views

--- client/src/views/ProductsDetailView.vue
+++ client/src/views/ProductsDetailView.vue
@@ -1,46 +1,39 @@
-<template>
-  <div>
-    <div class="products">
-      <RouterLink class="product" v-for="product in products" :key ='product.id' :to="`/product/${product.id}`">
-        <div class="product-image">
-          <img :src="product.thumbnail" :alt="product.title">
-        </div>
-        <div class="product-info">
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRoute } from "vue-router";
 
-        </div>
-      </RouterLink>
+import { getProduct } from "../stores/products";
+
+const route = useRoute();
+
+const product = ref(getProduct(+route.params.id));
+</script>
+
+<template>
+  <div v-if="product" class="section">
+    <div class="product-image">
+      <img :src="product.thumbnail" :alt="product.title" />
     </div>
+    <div class="product-info">
+      <h3 class="title">{{ product.title }}</h3>
+      <p class="price subtitle">
+        <span class="currency">$</span>
+        <span class="amount">{{ product.price }}</span>
+      </p>
+      <p>{{ product.description }}</p>
+    </div>
+  </div>
+  <div v-else class="section">
+    <h2 class="title">Product not found</h2>
   </div>
 </template>
 
-<script setup lang="ts">
-import { reactive } from "vue";
-import { getProducts } from "../stores/products";
-import { RouterLink } from "vue-router";
-
-const products = reactive(getProducts());
-</script>
-
 <style scoped>
-  .products{
-    display: flex;
-    flex-wrap: wrap;
-    background-color: aliceblue;
-  }
-  .product{
-    margin: 1em;
-    padding: 1em;
-    background-color: #fff;
-    flex-basis: 10em;
-    border: 1px solid #000;
-    border-radius: 5px;
-
-  }
-  .product-info{
-    font-size: small;
-  }
-  .price{
-    display: flex;
-
-  }
+.section {
+  display: flex;
+  background-color: aliceblue;
+}
+.product-info {
+  padding: 1em;
+}
 </style>
