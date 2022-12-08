5e34447b7163ff9ed90c341db30aa66815c2f711
KyleWendholt <101617205+KyleWendholt@users.noreply.github.com>
2022-12-08 14:11:09 -0500

    still need to update to current

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,19 +1,35 @@
-const express = require('express');
-const products = require('../models/products');
+const express = require("express");
+const products = require("../models/products");
 
 const app = express.Router();
 
-app.get('/', (req, res) => {
-  res.status(200).send(products.getProducts());
-}).get
-('/:id', (req, res) => {
-  
-  const product = products.getProduct(+req.params.id);
-  if (product) {
-    res.status(200).send(product);
-  } else {
-    res.status(404).send('Product not found');
-  }
-});
+app
+  .get("/", (req, res, next) => {
+    products
+      .getProducts()
+      .then((x) => res.status(200).send(x))
+      .catch(next);
+
+    res.status(200).send(products.getProducts());
+  })
+  .get("/:id", (req, res, next) => {
+    products
+      .getProduct(+req.params.id)
+      .then((x) => {
+        if (product) {
+          res.status(200).send(x);
+        } else {
+          res.status(404).send("Product not found");
+        }
+      })
+      .catch(next);
+
+    const product = products.getProduct(+req.params.id);
+    if (product) {
+      res.status(200).send(product);
+    } else {
+      res.status(404).send("Product not found");
+    }
+  });
 
 module.exports = app;
